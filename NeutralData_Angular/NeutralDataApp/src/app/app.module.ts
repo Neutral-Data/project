af3726802bee563be6a689f1455a5c05f14e7299
ismherram <ismherram@alum.us.es>
2023-10-30 22:47:48 +0100

    Add footer

--- NeutralData_Angular/NeutralDataApp/src/app/app.module.ts
+++ NeutralData_Angular/NeutralDataApp/src/app/app.module.ts
@@ -6,10 +6,20 @@ import { UserService } from './user.service';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    DashboardComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
---
+++ NeutralData_Angular/NeutralDataApp/src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../user';
+import { HttpErrorResponse } from '@angular/common/http';
+import { UserService } from '../../user.service';
+import { NgForm } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit{
+  public users: User[] = [];
+  public editUser: User;
+  public deleteUser: User;
+
+  constructor(private userService: UserService, private modalService: NgbModal){}
+
+  ngOnInit(): void {
+      this.getUsers();
+      this.modalService.dismissAll();
+  }
+
+  public getUsers(): void{
+    this.userService.getUsers().subscribe(
+      (response: User[]) => {
+        this.users = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onAddUser(addForm: NgForm): void{
+    document.getElementById('add-user-form').click();
+    this.userService.addUser(addForm.value).subscribe(
+      (response: User) => {
+        console.log(response);
+        this.getUsers();
+        addForm.reset();
+        window.location.reload();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public onUpdateUser(user: User): void {
+    this.userService.updateUser(user).subscribe(
+      (response: User) => {
+        console.log(response);
+        this.getUsers();
+        window.location.reload();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteUser(userId: number): void {
+    this.userService.deleteUser(userId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getUsers();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onOpenModal(user: User, mode: string): void{
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle','modal');
+    if (mode === 'add'){
+      button.setAttribute('data-target','#new-user-form-modal');
+    }
+    if (mode === 'edit'){
+      this.editUser = user;
+      button.setAttribute('data-target','#edit-user-form-modal');
+    }
+    if (mode === 'delete'){
+      this.deleteUser = user;
+      button.setAttribute('data-target','#delete-user-modal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+
+}
